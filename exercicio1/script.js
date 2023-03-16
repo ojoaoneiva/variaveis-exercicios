8cf2fb96dd3b451f9fe275df9545f5c5bdd5c940
João Neiva <jg.neiva@gmail.com>
2023-03-15 21:37:58 -0300

    exercícios finalizados e corrigidos

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,25 +1,26 @@
-let nome
-let idade
-console.log(typeof(nome))
-console.log(typeof(idade))
+// let nome
+// let idade
+//console.log(typeof nome)
+//console.log(typeof idade)
 
-//O resultado "objeto" aparece no console do DevTools já que o valor nulo foi inserido como valor da variavel. Portanto a única informação que a variavel carrega é de que é um objeto
+//O resultado undefined aparece no console do DevTools já que nenhum valor foi atribuido à variavel.
 
 nome = prompt("Qual o seu nome?")
 idade = prompt("Qual a sua idade?")
 
-console.log(typeof(nome))
-console.log(typeof(idade))
+//console.log(typeof(nome))
+//console.log(typeof(idade))
 // As duas são strings porque todo dados inserido no prompt é string
 
 console.log("Olá, ", nome+ " você tem "+ idade + " anos")
 
-const p1 = "Você está com uma camisa branca? "
-const p2 = "Você gosta de game of thrones? "
-const p3 = "Você está com sono? "
-const r1 = prompt(p1)
-const r2 = prompt(p2)
-const r3 = prompt(p3)
-console.log(p1, r1)
-console.log(p2, r2)
-console.log(p3, r3)
+const pergunta1 = "Você está com uma camisa branca?"
+const pergunta2 = "Você gosta de game of thrones? "
+const pergunta3 = "Você está ansioso?"
+const answer1 = prompt(pergunta1)
+const answer2 = prompt(pergunta2)
+const answer3 = prompt(pergunta3)
+
+console.log(pergunta1, answer1)
+console.log(pergunta2, answer2)
+console.log(pergunta3, answer3)
---
+++ exercicio3/script.js
@@ -0,0 +1,55 @@
+const pnome = "Digite se nome completo"
+const nome = prompt(pnome)
+console.log(typeof nome)
+
+const pnascimento = "Digite sua data de nascimento (utilize as / )"
+const nascimento = prompt(pnascimento)
+console.log(typeof nascimento)
+
+const pendereço ="Digite seu endereço"
+const endereço = prompt(pendereço)
+console.log(typeof endereço)
+
+const pcpf = "Digite seu cpf"
+const cpf = prompt(pcpf)
+console.log(typeof cpf)
+
+const pescolaridade = "Qual a sua escolaridade?"
+const escolaridade = prompt(pescolaridade)
+console.log(typeof escolaridade)
+
+const pcnh = "Você possui CNH?"
+const cnh = prompt(pcnh)
+console.log(typeof cnh)
+
+const pfilhes = "Quantos filhes você possui?"
+const filhes = prompt(pfilhes)
+console.log(typeof filhes)
+
+const pcargo = "Qual o seu cargo atual?"
+const cargo = prompt(pcargo)
+console.log(typeof cargo)
+
+const psalario = "Qual o seu salário em reais?"
+const salario = prompt(psalario)
+console.log(typeof salario)
+
+const pcomissao = "Recebe comissão? Se sim, informe a porcentagem, caso não, responda com zero"
+const comissao = prompt(pcomissao)
+console.log(typeof comissao)
+
+const padmissao = "Informe o ano de admissão"
+const admissao = prompt(padmissao)
+console.log(typeof admissao)
+
+console.log(pnome, ": ", nome)
+console.log(pnascimento, ": ", nascimento)
+console.log(pendereço, ": ", endereço)
+console.log(pcpf, ": ", cpf)
+console.log(pescolaridade, ": ", escolaridade)
+console.log(pcnh, ": ", cnh)
+console.log(pfilhes, ": ", filhes)
+console.log(pcargo, ": ", cargo)
+console.log(psalario, ": ", salario)
+console.log(pcomissao, ": ", comissao)
+console.log(padmissao, ": ", admissao)
